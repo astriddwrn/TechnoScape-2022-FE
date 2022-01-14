43cd604bc7ad25afec13a8c402be8741e9f10dad
astriddwrn <astrid.dewi.rana@gmail.com>
2022-01-15 00:14:44 +0700

    [patch] tailwind: configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
     content: ["./*.html"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                cBtnGradientCS: 'linear-gradient(90deg, #547EE2 0%, #5DA0FA 100%)',
+                cLinearGradient: 'linear-gradient(90deg, rgba(84, 126, 226, 0.7) 0%, rgba(93, 160, 250, 0.7) 100%)',
+                cBgLight: '#F3F3FF',
+                cGray: '#6A6E72',
+                cWhite: '#FFFFFF',
+                cDarkerGray: '#595959',
+            },
+            boxShadow: {
+                'bcInput': '5px 7px 25px 5px rgba(213, 213, 213, 0.16)',
+
+            },
+            fontFamily: {
+                'sans' : ['PJSRegular','Helvetica', 'Arial', 'sans-serif'],
+                'PJSItalic': ['PJSItalic'],
+                'PJSExtraBold' : ['PJSExtraBold'],
+                'PJSExtraBoldItalic': ['PJSExtraBoldItalic'],
+                'PJSBold' : ['PJSBold'],
+                'PJSBoldItalic': ['PJSBoldItalic'],
+                'PJSSemiBold' : ['PJSSemiBold'],
+                'PJSSemiBoldItalic' : ['PJSSemiBoldItalic'],
+                'PJSMedium' : ['PJSMedium'],
+                'PJSMediumItalic' : ['PJSMediumItalic'],
+                'PJSLight' : ['PJSLight'],
+                'PJSLightItalic' : ['PJSLightItalic'],
+                'PJSExtraLight' : ['PJSExtraLight'],
+                'PJSExtraLightItalic' : ['PJSExtraLightItalic'],
+            }
+        },
     },
     variants:{
         extends:{}
